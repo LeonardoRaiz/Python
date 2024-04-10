dbc3bfdd1ca4ea1faad78a67aacbbd88cda59a48
Leonardo <lhraiz@cpqd.com.br>
2024-04-10 08:44:40 -0300

    Novas aulas

--- Aula 02/ex2.py
+++ Aula 02/ex2.py
@@ -1,5 +1,13 @@
-nome = input('Qual o seu nome? ')
-altura = input('Qual a sua altura? ')
-peso = input('Qual o seu peso? ')
-imc = ... # Placeholder de código em python # IMC = peso / (altura x altura)
+nome = 'Léo Raiz'
+altura = 1.93
+peso = 105
+imc = peso / altura ** 2
 
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
